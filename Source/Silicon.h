ab5f1383ad87fbe9df68debeb611144d0ecca9dc
Mateusz Malczak <mateusz@malczak.info>
2017-11-28 11:05:19 +0100

    update project to swift4

--- Source/Silicon.h
+++ Source/Silicon.h
@@ -13,7 +13,3 @@ FOUNDATION_EXPORT double SiliconVersionNumber;
 
 //! Project version string for Silicon.
 FOUNDATION_EXPORT const unsigned char SiliconVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Silicon/PublicHeader.h>
-
-
